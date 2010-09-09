85256b569ddc74d7f9f4ba6e0e187684ad32fe90
Sora Harakami <sora134@gmail.com>
2010-09-10 07:16:39 +0900

    now pass test

--- ext/prime/prime.c
+++ ext/prime/prime.c
@@ -8,11 +8,13 @@
 
 #include "ruby/ruby.h"
 
+VALUE prime_um_value;
+
 static VALUE
 prime_is_value_prime(int argc, VALUE *argv, VALUE self) {
-    int step23;
-    long int i,x;
-    VALUE value, generator;
+    int step23, v;
+    long int i, x;
+    VALUE value, generator, t, iv;
 
     rb_scan_args(argc, argv, "11", &value, &generator);
 
@@ -21,44 +23,58 @@ prime_is_value_prime(int argc, VALUE *argv, VALUE self) {
 
     if(!(FIXNUM_P(value) || TYPE(value) == T_BIGNUM))
 	rb_raise(rb_eTypeError, "value must be a numeric");
+    if(!prime_um_value)
+	prime_um_value = ULONG2NUM(ULONG_MAX);
 
-    x = NUM2LONG(value);
+    if (v = (rb_funcall(value,rb_intern(">"),1,prime_um_value) == Qtrue)){
+	if (rb_funcall(value,rb_intern("<"),1,INT2FIX(2)) == Qtrue)
+	    return Qfalse;
+	if (rb_funcall(value,rb_intern("=="),1,INT2FIX(2)) == Qtrue ||
+	    rb_funcall(value,rb_intern("=="),1,INT2FIX(3)) == Qtrue)
+	    return Qtrue;
+    } else {
+	x = NUM2LONG(value);
+	if (x < 0)  x = x * -1;
 
-    if(x < 2) return Qfalse;
-
+	if (x <  2) return Qfalse;
+	if (x == 2) return Qtrue;
+	if (x == 3) return Qtrue;
+    }
     if(NIL_P(generator)) { /* generator = Prime::Generator23.new */
 	step23 = 0;
 	i = 1;
-	while(i < x) {
-	    printf("%ld %d before switch\n", i, step23);
+	while(1) {
 	    if (step23 < 1) {
 		switch(i) {
 		    case 1:
-			printf("case 1\n");
 			i = 2;
-			printf("%ld %d in switch\n", i, step23);
 			break;
 		    case 2:
-			printf("case 2\n");
 			i = 3;
 			break;
 		    case 3:
-			printf("case 3\n");
 			i = 5;
 			step23 = 2;
 			break;
 		}
 	    }else{
-		printf("non! switch!!\n");
 		i += step23;
 		step23 = 6 - step23;
 	    }
-	    printf("%ld %d after switch\n", i, step23);
-
-	    if (x % i == 0)
-		return Qfalse;
+	    if (v) {
+		iv = ULONG2NUM(i);
+		t = rb_funcall(value,rb_intern("divmod"),1,iv);
+		if (rb_funcall(rb_ary_shift(t),rb_intern("<"),1,iv) == Qtrue)
+		    return Qtrue;
+		if (rb_funcall(rb_ary_shift(t),rb_intern("=="),1,INT2FIX(0)) == Qtrue)
+		    return Qfalse;
+	    } else {
+		if (x / i < i)
+		    return Qtrue;
+		if (x % i == 0)
+		    return Qfalse;
+	    }
 	}
-	return Qtrue;
     }else{
 	return Qfalse; /* NOTE: fix this */
     }
